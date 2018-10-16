e6b0e37ef4058e7daffe6502fae5203f07519883
Gianluca Capozzi <capozzigianluca96@gmail.com>
2018-10-16 20:42:53 +0200

    Migrate degli utenti

--- iMedical/db/schema.rb
+++ iMedical/db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_163859) do
+ActiveRecord::Schema.define(version: 2018_10_16_183200) do
+
+  create_table "doctors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "secretaries", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
