c8a71fb358c8e956b447b67c60642a1739662e6c
Gerardo Audino <gerardoaudino@MacBook-Air-di-Gerardo.local>
2018-10-23 19:30:26 +0200

    Aggiunta funzione: seleziona studio medico (segretario)

--- iMedical/app/models/secretary.rb
+++ iMedical/app/models/secretary.rb
@@ -1,6 +1,7 @@
 class Secretary < User
 
-    has_many :manages
+   has_many :manages
+   has_many :clinics, through: :manages
 
     def self.search(search_term)
         where("LOWER(name) LIKE ?", "%#{search_term.downcase}%")
