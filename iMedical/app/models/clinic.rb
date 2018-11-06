bec87895aaf22e3bd04652dc45f79ef779e5af9c
Gianluca Capozzi <capozzigianluca96@gmail.com>
2018-11-06 09:20:09 +0100

    Fixed maps problem

--- iMedical/app/models/clinic.rb
+++ iMedical/app/models/clinic.rb
@@ -7,12 +7,7 @@ class Clinic < ApplicationRecord
     has_many :secretaries, through: :manages
 
 
-    geocoded_by :full_address
-    after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
-
-    def full_address
-        ["Italy", city, address].compact.join(',')
-    end
+    geocoded_by :address
 
     def self.search(search_term)
         where("LOWER(name) LIKE ?", "%#{search_term.downcase}%")
